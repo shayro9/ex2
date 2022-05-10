81be5a5b65b7f4c0c114e43b07a438ff1ca4e5b1
michalmaya <michal.maya75@gmail.com>
2022-05-10 18:39:59 +0300

    h

--- Player.h
+++ Player.h
@@ -5,4 +5,38 @@
 #ifndef EX2_PLAYER_H
 #define EX2_PLAYER_H
 
-#endif //EX2_PLAYER_H
+
+#include <string>
+#include "Player.h"
+#include "utilities.h"
+
+class Player {
+public:
+    Player (char* name, int maxHP=100, int force=5);
+    ~Player ();
+    Player (const Player& player);
+    Player& operator= (const Player& player);
+    void printInfo();
+    void levelUp();
+    int getLevel();
+    void buff(int amount);
+    void heal(int amount);
+    void damage(int amount);
+    bool isKnockedOut ();
+    void addCoins (int amount);
+    bool pay (int amount);
+    int getAttackStrength ();
+
+
+
+private:
+    char* name;
+    int level
+    int force;
+    int maxHP;
+    int HP;
+    int coins;
+
+};
+
+#endif //EX2_Player_H

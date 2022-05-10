9807f000d2fa9efc64bd365165cb4210b81a3bf6
michalmaya <michal.maya75@gmail.com>
2022-05-10 17:46:12 +0300

    player cpp+h

---
+++ Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by michalmaya on 10/05/2022.
+//
+
+#include "Player.h"
---
+++ Player.h
@@ -0,0 +1,8 @@
+//
+// Created by michalmaya on 10/05/2022.
+//
+
+#ifndef EX2_PLAYER_H
+#define EX2_PLAYER_H
+
+#endif //EX2_PLAYER_H
